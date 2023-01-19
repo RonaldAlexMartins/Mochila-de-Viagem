845fa6b350976fd77b68db7bdefbd5073ff11cd4
RonaldAlexMartins <ronny_nascimento@hotmail.com>
2023-01-19 15:08:57 -0300

    armazenando no localstorage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const itens = []
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault()
+  const nome = evento.target.elements['nome']
+  const quantidade = evento.target.elements['quantidade']
 
-  criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+  criaElemento(nome.value, quantidade.value)
+  nome.value = ""
+  quantidade.value = ""
 })
 
 function criaElemento (nome, quantidade){
@@ -19,8 +24,14 @@ function criaElemento (nome, quantidade){
 
   novoItem.appendChild(numeroItem)
   novoItem.innerHTML += nome
+
   lista.appendChild(novoItem)
 
-  
+  const itemAtual = {
+    "nome": nome,
+    "quantidade": quantidade,
+  }
+  itens.push(itemAtual)
+  localStorage.setItem("item", JSON.stringify(itemAtual))
 
 }
