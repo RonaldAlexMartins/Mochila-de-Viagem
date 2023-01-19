b8e17ebe90d8689f89609b1d00bc8d0432e16b89
RonaldAlexMartins <ronny_nascimento@hotmail.com>
2023-01-19 15:12:37 -0300

    organizando o codigo

--- js/main.js
+++ js/main.js
@@ -4,25 +4,24 @@ const itens = []
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault()
+
   const nome = evento.target.elements['nome']
   const quantidade = evento.target.elements['quantidade']
 
   criaElemento(nome.value, quantidade.value)
+
   nome.value = ""
   quantidade.value = ""
 })
 
 function criaElemento (nome, quantidade){
-  console.log(nome)
-  console.log(quantidade)
-
   const novoItem = document.createElement('li')
   novoItem.classList.add("item")
 
   const numeroItem = document.createElement('strong')
   numeroItem.innerHTML = quantidade
-
   novoItem.appendChild(numeroItem)
+
   novoItem.innerHTML += nome
 
   lista.appendChild(novoItem)
@@ -31,7 +30,8 @@ function criaElemento (nome, quantidade){
     "nome": nome,
     "quantidade": quantidade,
   }
-  itens.push(itemAtual)
-  localStorage.setItem("item", JSON.stringify(itemAtual))
 
+  itens.push(itemAtual)
+
+  localStorage.setItem("itens", JSON.stringify(itens))
 }
